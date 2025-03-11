7d4bdbfadd978c7f914ad00690f031d0a8f63933
xubufubu <xubufubu@gmail.com>
2025-03-11 03:29:06 -0400

    Add logic for 1 round of game
    I ordered the options by outcomes of win, draw, and loss.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 100)
 
-    console.log(randomNumber)
-
     if (randomNumber % 3 === 0) {
         return "scissors";
     } else if (randomNumber % 5 === 0) {
@@ -15,26 +13,47 @@ function getComputerChoice() {
 
 }
 
-
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
 
 
 function getHumanChoice() {
     let userChoice = prompt("rock, paper, or scissors?")
+    return userChoice.toLowerCase()
 
-    if (userChoice == "rock") {
-        return "rock";
-    } else if (userChoice == "paper") {
-        return "paper";
-    } else if (userChoice == "scissors") {
-        return "scissors";
+}
+
+// console.log(getHumanChoice())
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    let a = humanChoice;
+    console.log(a)
+    let b = computerChoice;
+    console.log(b)
+
+
+    if (a == "rock" && b == "scissors" || a == "scissors" && b == "paper" || a == "paper" && b == "rock") {
+        return humanScore++
+    } else if (a == "rock" && b == "rock" || a == "paper" && b == "paper" || a == "scissors" && b == "scissors") {
+        return console.log("Draw")
+    } else if (a == "rock" && b == "paper" || a == "paper" && b == "scissors" || a == "scissors" && b == "rock") {
+        return computerScore++
     } else {
-        return "something bad happened.";
+        return "error";
     }
 }
 
-console.log(getHumanChoice())
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 
+playRound(humanSelection, computerSelection);
 
 
+console.log(humanScore, " ", computerScore)
